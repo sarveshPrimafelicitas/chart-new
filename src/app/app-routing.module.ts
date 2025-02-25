7c7972490246e2b74caf9d01c234db29f5468de5
sarveshPrimafelicitas <sarvesh.k@primafelicitas.co>
2025-02-25 10:04:02 +0530

    dsad

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+// import { NgModule } from '@angular/core';
+// import { RouterModule, Routes } from '@angular/router';
+// import { AppComponent } from './app.component';
+// import { ShowChartComponent } from './show-chart/show-chart.component';
+
+// const routes: Routes = [
+//   { path: '', component: AppComponent }, // Default route
+//   { path: 'chart', component: ShowChartComponent }
+
+// ];
+
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule]
+// })
+// export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ShowChartComponent } from './show-chart/show-chart.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, ShowChartComponent],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
